15ecebf1e884803cb1d7fd4f71dd5da7d48b1310
glucciani <giancarlolucciani@gmail.com>
2020-04-18 17:15:17 -0300

    Criando apagina mensagem e atribundindo o redirect no controller

--- src/main/java/com/lucciani/cobranca/controller/TituloController.java
+++ src/main/java/com/lucciani/cobranca/controller/TituloController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lucciani.cobranca.model.StatusTitulo;
 import com.lucciani.cobranca.model.Titulo;
@@ -18,23 +21,53 @@ import com.lucciani.cobranca.repository.TitulosRepository;
 @RequestMapping("/titulos")
 public class TituloController {
 
+	/*
+	 * @titulosRepository: Injeta a dependencia do Objeto repository
+	 * 
+	 */
+	
 	@Autowired
 	private TitulosRepository titulosRepository;
 
+	/*
+	 * @mv: retorna um objeto ModelAndView cadastro-titulo
+	 * 
+	 */
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cadastro-titulo");
+		mv.addObject(new Titulo());
 		return mv;
 	}
+	
+	/*
+	 * @titulo: Valida o objeto na entidade
+	 * 
+	 * @erros: Trata o erro relacionado a validações
+	 * 
+	 * @attributes: Envia a mensagem de sucesso na sessão e faz o redirect para a nova pagina
+	 * 
+	 * 
+	 */
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
+	public String salvar(@Validated Titulo titulo, Errors erros, RedirectAttributes attributes) {
+		if(erros.hasErrors()) {
+			return "cadastro-titulo";
+		}
+		
 		titulosRepository.save(titulo);
 
-		ModelAndView mv = new ModelAndView("cadastro-titulo");
-		mv.addObject("mensagem", "Titulo salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
+	
+	/*
+	 * 
+	 * 
+	 * 
+	 */
 
 	@RequestMapping
 	public ModelAndView pesquisar() {
@@ -43,6 +76,12 @@ public class TituloController {
 		mv.addObject("titulos", todosTitulos);
 		return mv;
 	}
+	
+	/*
+	 * 
+	 * 
+	 * 
+	 */
 
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
--- src/main/java/com/lucciani/cobranca/model/Titulo.java
+++ src/main/java/com/lucciani/cobranca/model/Titulo.java
@@ -11,6 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -18,20 +21,53 @@ import org.springframework.format.annotation.NumberFormat;
 @Entity
 public class Titulo {
 
-	
+	/*
+	 * @id
+	 * 
+	 * @GeneratedValue
+	 * 
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-	
+
+	/*
+	 * @NotBlack
+	 * 
+	 * @Size
+	 * 
+	 */
+	@NotBlank(message = "O campo NOME é obrigatório")
+	@Size(max = 50, message = "O nome não pode conter mais de 50 caracteres")
 	private String nome;
-	
+
+	/*
+	 * @NotNull
+	 * 
+	 * @DateTimeFormat
+	 * 
+	 * @Temporal
+	 * 
+	 */
+	@NotNull(message = "Data de vencimento é obrigatório")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
-	
+
+	/*
+	 * 
+	 * @NumberFormat
+	 * 
+	 */
+	@NotNull(message = "O campo VALOR é obrigatorio")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
-	
+
+	/*
+	 * 
+	 * @Enumerated
+	 * 
+	 */
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
 
@@ -74,8 +110,8 @@ public class Titulo {
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
 	}
-	
-	//Metodo para validar o status pendente do titulo
+
+	// Metodo para validar o status pendente do titulo
 	public boolean isPendente() {
 		return StatusTitulo.PENDENTE.equals(this.status);
 	}
